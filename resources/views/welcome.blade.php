395a340a4301ec8169a122717db3b4302083ab09
Jesús Domingo Mestra Vega <jdmestrav@grupokonecta.com>
2019-03-13 17:03:34 -0500

    Se añade menú con opciones principales

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
   <div class="p-0 m-0 app_container_overlay bg-overlay d-flex align-items-center justify-content-center">
     <div class="login_container py-2 px-3 rounded d-flex flex-column align-items-center">
           <i class="fas fa-user-circle fa-3x my-2"></i>
-          <h4 class="text-center"> ¡Bienvenido User! </h4>
+          <h4 class="text-center"> ¡Bienvenido Usuario invitado! </h4>
           <p class="text-center">¡Continua para vivir la mejor experiencia en compras online!</p>
           <a href="{{ route('user.home') }}" class="btn btn-success my-2 mx-4"> <i class="fas fa-sign-in-alt"></i> ¡empieza a comprar! </a>
     </div>
