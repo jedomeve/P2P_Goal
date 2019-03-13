24bd85e0c3d17ecc33ec16064451cd7aa754cdb5
Jesús Domingo Mestra Vega <jdmestrav@grupokonecta.com>
2019-03-12 21:08:22 -0500

    Se añaden certificados y documentación para la configuración de certificados SSL.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# P2P_Goal
+# P2P_basic_payment
+
+**Autor:** Jesús Domingo Mestra Vega
+
+**fecha:** 12.03.2019
+
+
+
+## Requerimientos
+
+Este es el código fuente desarrollado para realizar una conexión a la Api de redirección de PlaceToPay y realizar la función de pago básico que incluye. para la realización de esta prueba fue necesario tener un ambiente de desarrollo con las siguientes carácteristicas:
+
+
+
+- **wamppServer 3.1.3 de 64 Bits**: Una plataforma que incluye el servicios como Apache, MySQL y compilación de diferentes versiones de PHP.
+- **php 7.2.4**
+- **laravel 5.8 (versión mas reciente)**
+
+Para la ejecución del framework laravel con el servidor web Apache es necesario seguirlos pasos indicados en la documentación del framework que incluye la configuración del VirtualHost habilitando el módulo Rewrite. 
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,51 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+  $login = "6dd490faf9cb87a9862245da41170ff2";
+  $secretKey = "024h1IlD";
+  $seed = date('c'); ;
+
+  if (function_exists('random_bytes')) {
+      $nonce = bin2hex(random_bytes(16));
+  } elseif (function_exists('openssl_random_pseudo_bytes')) {
+      $nonce = bin2hex(openssl_random_pseudo_bytes(16));
+  } else {
+      $nonce = mt_rand();
+  }
+
+  $nonceBase64 = base64_encode($nonce);
+
+  $tranKey = base64_encode(sha1($nonce . $seed . $secretKey, true));
+
+    $placetopay = new Dnetix\Redirection\PlacetoPay([
+        'login' => $login,
+        'tranKey' => $tranKey,
+        'url' => 'https://test.placetopay.com/redirection/',
+    ]);
+
+    $reference = "fact-01";
+    $request = [
+      'payment' => [
+          'reference' => $reference,
+          'description' => 'Testing payment',
+          'amount' => [
+              'currency' => 'USD',
+              'total' => 120,
+          ],
+      ],
+      'expiration' => date('c', strtotime('+2 days')),
+      'returnUrl' => route( 'payment' , [ 'reference' => $reference] ),
+      'ipAddress' => '127.0.0.1',
+      'userAgent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
+    ];
+  
+    $response = $placetopay->request($request);
+    var_dump($response);
+
+
 });
+
+Route::get('/end-payment/{reference}', function($reference=null){
+  var_dump($reference);
+})->name('payment');
